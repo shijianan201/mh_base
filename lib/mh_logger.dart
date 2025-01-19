979ecfe7bde3ae2b5b714081376fceb9653b4db7
shijianan201 <xiaojiangagaga@gmail.com>
2025-01-20 00:31:15 +0800

    first commit

---
+++ lib/mh_logger.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart';
+import 'package:logger/logger.dart';
+
+void logD(dynamic message,
+    {DateTime? time, Object? error, StackTrace? stackTrace}) {
+  MhLogger.d(message, time: time, error: error, stackTrace: stackTrace);
+}
+
+void logE(dynamic message,
+    {DateTime? time, Object? error, StackTrace? stackTrace}) {
+  MhLogger.e(message, time: time, error: error, stackTrace: stackTrace);
+}
+
+class MhLogger {
+  static final _logger = Logger(
+      printer: PrettyPrinter(
+          methodCount: 2,
+          errorMethodCount: 0,
+          lineLength: 120,
+          colors: true,
+          printEmojis: true,
+          dateTimeFormat: DateTimeFormat.onlyTimeAndSinceStart),
+      filter: _MhLogFilter());
+
+  static void d(dynamic message,
+      {DateTime? time, Object? error, StackTrace? stackTrace}) {
+    _logger.d(message, time: time, error: error, stackTrace: stackTrace);
+  }
+
+  static void e(dynamic message,
+      {DateTime? time, Object? error, StackTrace? stackTrace}) {
+    _logger.e(message, time: time, error: error, stackTrace: stackTrace);
+  }
+}
+
+class _MhLogFilter extends LogFilter {
+  @override
+  bool shouldLog(LogEvent event) {
+    return kProfileMode || kDebugMode;
+  }
+}
