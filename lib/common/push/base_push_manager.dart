476ca43633c823354c4a14fb9be7b51f0c90b252
shijianan <shijianan@ksztone.com>
2025-07-20 18:14:21 +0800

    掌紋

--- lib/common/push/base_push_manager.dart
+++ lib/common/push/base_push_manager.dart
@@ -20,19 +20,26 @@ class BasePushManager<T extends BaseUserProfile> extends UserListener<T> {
     if (hasSetup) {
       return;
     }
-    _jPush.setAuth(enable: true);
-    _jPush.setup(
-        appKey: appKey, production: production, channel: channel, debug: debug);
-    _jPush.applyPushAuthority(NotificationSettingsIOS());
-    _jPush.addEventHandler(onReceiveNotification: (a) async {
-      for (var e in _onReceiveNotificationListeners) {
-        e.onReceiveNotification(a);
-      }
-    }, onOpenNotification: (a) async {
-      for (var e in _onOpenNotificationListeners) {
-        e.onOpenNotification(a);
-      }
-    });
+    try {
+      _jPush.setAuth(enable: true);
+      _jPush.setup(
+          appKey: appKey,
+          production: production,
+          channel: channel,
+          debug: debug);
+      _jPush.applyPushAuthority(NotificationSettingsIOS());
+      _jPush.addEventHandler(onReceiveNotification: (a) async {
+        for (var e in _onReceiveNotificationListeners) {
+          e.onReceiveNotification(a);
+        }
+      }, onOpenNotification: (a) async {
+        for (var e in _onOpenNotificationListeners) {
+          e.onOpenNotification(a);
+        }
+      });
+    }catch(e){
+
+    }
     hasSetup = true;
   }
 
@@ -53,10 +60,14 @@ class BasePushManager<T extends BaseUserProfile> extends UserListener<T> {
     _onOpenNotificationListeners.remove(listener);
   }
 
-  Future<String> getRegistrationId() async {
+  Future<String?> getRegistrationId() async {
+    try {
     var res = await _jPush.getRegistrationID();
     "jpush registration id = $res".logD();
     return res;
+    } catch (e) {
+      return null;
+    }
   }
 
   Future<Map<dynamic, dynamic>> setAlias(String alias) async {
@@ -64,7 +75,8 @@ class BasePushManager<T extends BaseUserProfile> extends UserListener<T> {
       return {};
     }
     try {
-      return _jPush.setAlias(alias);
+      var res = await _jPush.setAlias(alias);
+      return res;
     } catch (e) {
       return {"errorCode": -1};
     }
@@ -75,18 +87,28 @@ class BasePushManager<T extends BaseUserProfile> extends UserListener<T> {
       return {};
     }
     try {
-      return _jPush.deleteAlias();
+      var res = await _jPush.deleteAlias();
+      return res;
     } catch (e) {
       return {"errorCode": -1};
     }
   }
 
-  Future<dynamic> stopPush() {
-    return _jPush.stopPush();
+  Future<dynamic> stopPush() async {
+    try {
+      var res = await _jPush.stopPush();
+      return res;
+    }catch (e) {
+      return null;
+    }
   }
 
-  Future<dynamic> resumePush() {
-    return _jPush.resumePush();
+  Future<dynamic> resumePush() async {
+    try {
+      return _jPush.resumePush();
+    }catch (e) {
+      return null;
+    }
   }
 
   @override
